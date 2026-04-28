3b8bb41b80bef689c8a045d4da413682f12080ef
m.osipov <m.osipov@slotegrator.space>
2026-04-28 23:13:10 +0200

    init

---
+++ C.Training/BasicTasks.cs
@@ -0,0 +1,40 @@
+﻿namespace C.Training;
+
+public static class BasicTasks
+{
+    // Задача 1
+    public static string SayHello()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Задача 2
+    public static string FormatPerson(string name, int age)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Задача 3
+    public static int Sum(int a, int b)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Задача 4
+    public static int Square(int number)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Задача 5
+    public static bool IsEven(int number)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Задача 6
+    public static int SumToN(int n)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ C.Training/BasicTasksTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+
+namespace C.Training;
+
+public class BasicTasksTests
+{
+    [Fact]
+    public void SayHello_ShouldReturnCorrectString()
+    {
+        var result = BasicTasks.SayHello();
+
+        Assert.Equal("Привет, я учу C#!", result);
+    }
+
+    [Fact]
+    public void FormatPerson_ShouldFormatCorrectly()
+    {
+        var result = BasicTasks.FormatPerson("Михаил", 25);
+
+        Assert.Equal("Меня зовут Михаил, мне 25", result);
+    }
+
+    [Fact]
+    public void Sum_ShouldReturnCorrectSum()
+    {
+        var result = BasicTasks.Sum(2, 3);
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Square_ShouldReturnCorrectSquare()
+    {
+        var result = BasicTasks.Square(4);
+
+        Assert.Equal(16, result);
+    }
+
+    [Fact]
+    public void IsEven_ShouldReturnTrue_ForEven()
+    {
+        var result = BasicTasks.IsEven(4);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsEven_ShouldReturnFalse_ForOdd()
+    {
+        var result = BasicTasks.IsEven(5);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void SumToN_ShouldWorkCorrectly()
+    {
+        var result = BasicTasks.SumToN(5);
+
+        Assert.Equal(15, result); // 1+2+3+4+5
+    }
+}
