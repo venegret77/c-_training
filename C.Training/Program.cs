13d673e8f9e894a319aedc042244c08824c3a93f
m.osipov <m.osipov@slotegrator.space>
2026-06-23 21:45:20 +0200

    homework lesson 2

--- C.Training/Program.cs
+++ C.Training/Program.cs
@@ -1,18 +1,60 @@
 ﻿Console.WriteLine("Hello this is Rock Paper Scissors");
-Console.WriteLine("Enter your step");
 
+Console.WriteLine("Enter your rounds count:");
+var userInputRoundsCount = Console.ReadLine();
 
-var userWon = false;
+int roundsCount;
 
-while (userWon == false)
+while (!int.TryParse(userInputRoundsCount, out roundsCount) || roundsCount <= 0)
 {
     Console.Clear();
     
+    Console.WriteLine("Invalid input");
+    Console.WriteLine("Enter your rounds count:");
+    userInputRoundsCount = Console.ReadLine();
+}
+
+Console.WriteLine("Enter your step");
+
+
+var userWinsCount = 0;
+var userLoseCount = 0;
+var userDrawCount = 0;
+
+while (userWinsCount + userLoseCount + userDrawCount <= roundsCount)
+{
+    Console.Clear();
+
+    if (userWinsCount != 0 || userLoseCount != 0)
+    {
+        Console.WriteLine($"You wins {userWinsCount}");
+        Console.WriteLine($"Computer wins {userLoseCount}");
+    }
+
+    if (userWinsCount > (roundsCount - userDrawCount) / 2)
+    {
+        Console.WriteLine("You won!");
+        
+        Thread.Sleep(3000); //чтобы увидеть сообщение выше
+        
+        return;
+    }
+    
+    if (userLoseCount > (roundsCount - userDrawCount) / 2)
+    {
+        Console.WriteLine("You loose(");
+        
+        Thread.Sleep(3000); //чтобы увидеть сообщение выше
+        
+        return;
+    }
+
     Console.WriteLine("1 - Rock");
     Console.WriteLine("2 - Paper");
     Console.WriteLine("3 - Scissors");
+    Console.WriteLine("4 - Well");
     Console.WriteLine("0 - Exit");
-    
+
     var userInput = Console.ReadLine(); // "5"
 
     int userChoice;
@@ -21,7 +63,7 @@ while (userWon == false)
     // put result to out result param
     // return if parse was successful
 
-    if (!int.TryParse(userInput, out userChoice) || !(userChoice >= 0 && userChoice <= 3))
+    if (!int.TryParse(userInput, out userChoice) || !(userChoice >= 0 && userChoice <= 4))
     {
         Console.WriteLine("Invalid input");
         continue;
@@ -33,7 +75,7 @@ while (userWon == false)
     }
 
     var random = new Random();
-    var computerChoice = random.Next(1, 4); // generate random number 1-3
+    var computerChoice = random.Next(1, 5); // generate random number 1-4
 
     string userChoiceString;
     switch (userChoice)
@@ -45,8 +87,11 @@ while (userWon == false)
         case 2:
             userChoiceString = "Paper";
             break;
-        default:
+        case 3:
             userChoiceString = "Scissors";
+            break;
+        default:
+            userChoiceString = "Well";
             break;
     }
 
@@ -56,7 +101,8 @@ while (userWon == false)
     {
         1 => "Rock",
         2 => "Paper",
-        _ => "Scissors"
+        3 => "Scissors",
+        _ => "Well"
     };
 
     Console.WriteLine($"Computer chose {computerChoiceString}");
@@ -64,17 +110,23 @@ while (userWon == false)
     if (computerChoice == userChoice)
     {
         Console.WriteLine("Draw");
+        userDrawCount++;
     }
     else if (userChoice == 1 && computerChoice == 3 || userChoice == 2 && computerChoice == 1 ||
-             userChoice == 3 && computerChoice == 2)
+             userChoice == 3 && computerChoice == 2
+             || userChoice == 4 && computerChoice == 1
+             || userChoice == 4 && computerChoice == 3)
     {
         Console.WriteLine("You win");
-        userWon = true;
+        userWinsCount++;
     }
     else
     {
         Console.WriteLine("You lose");
+        userLoseCount++;
     }
+    
+    Thread.Sleep(3000); // прочитал в интернете, что так можно сделать паузу в работе, чтобы видеть результаты раунда до очистки консоли
 
     // Ctrl + K + D 
 }
